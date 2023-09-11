6ae5c4a18fda3b511b9beb373748683682dee945
Amandeep Kaur <000822179@mohawkcollege.ca>
2023-09-10 21:16:31 -0400

    Added Reviews section, need to fix the add reviews

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import {useState,useEffect} from 'react';
 import Layout from './components/layout.js';
 import {Routes,Route,Link} from 'react-router-dom';
 import Home from './components/home/Home.js';
+import Header from './components/header/Header';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
+
+
   const [recipes,setRecipes] = useState([]);
+  const [recipe,setRecipe] = useState();
+  const [reviews,setReviews] = useState([]);
+
   const getRecipes = async() =>{
     try{
       const response = await api.get("api/v1/recipes");
@@ -18,15 +25,30 @@ function App() {
     }
     
   }
+
+  const getRecipeData = async(recipeId) =>{
+    try{
+      const response = await api.get(`api/v1/recipes/${recipeId}`);
+      const singleRecipe = response.data;
+      setRecipe(singleRecipe);
+      setReviews(singleRecipe.reviewIds);
+
+    }catch(error){
+
+    }
+  }
+
   useEffect(() => {
     getRecipes();
   },[]);
   return (
     <div className="App">
+      <Header/>
       <Link to="/home">Home</Link>
       <Routes>
         <Route path="/" element={<Layout/>}>Layout</Route>
         <Route path="/home" element={<Home recipes={recipes}/>}>Home</Route>
+        <Route path="/Reviews/:recipeId" element={<Reviews getRecipeData={getRecipeData} recipe={recipe} reviews={reviews} setReviews={setReviews} />}>Reviews</Route>
       </Routes>
     </div>
   );
--- src/components/food/Food.js
+++ src/components/food/Food.js
@@ -2,8 +2,17 @@ import React from 'react';
 import Carousel from 'react-material-ui-carousel';
 import {Paper} from '@mui/material';
 import './Food.css';
+import { useNavigate } from 'react-router-dom';
+import Reviews from '../reviews/Reviews';
+import Button from 'react-bootstrap/Button';
 
 const Food = ({recipes}) => {
+
+    const navigate = useNavigate();
+    function reviews(recipeId){
+        navigate(`/Reviews/${recipeId}`);
+    }
+
     return (
         <div className='recipe-carousel-container'>
             <Carousel>
@@ -20,12 +29,12 @@ const Food = ({recipes}) => {
                                             <div className='recipe-title'>
                                                 <h4>{recipe.title}</h4>
                                             </div>
-                                            <div className='recipe-ingredients'>
-                                                <h4>{recipe.ingredients}</h4>
-                                            </div>
                                             <div className='recipe-instructions'>
                                                 <h4>{recipe.instructions}</h4>
                                             </div>
+                                        </div>
+                                        <div className="movie-review-button-container">
+                                            <Button variant="info" onClick={() => reviews(recipe.id)}>Reviews</Button>
                                         </div>
                                     </div>
                                 </div>
